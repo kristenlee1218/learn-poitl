d637fb4f0b999a2938f4fbeb51902c369f037bda
kristen <1065200748@qq.com>
2022-06-07 11:23:00 +0800

    2022.6.7

--- src/main/java/com/learn/chapter1/Test1.java
+++ src/main/java/com/learn/chapter1/Test1.java
@@ -2,7 +2,6 @@ package com.learn.chapter1;
 
 import com.deepoove.poi.XWPFTemplate;
 
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.HashMap;
@@ -10,15 +9,17 @@ import java.util.HashMap;
 /**
  * @author ：Kristen
  * @date ：2022/6/6
- * @description :
+ * @description : Template 模板、Data-model 数据、Output 输出
  */
 public class Test1 {
     public static void main(String[] args) throws IOException {
+        // 编译模板、渲染数据
         XWPFTemplate template = XWPFTemplate.compile("D:\\template.docx").render(
                 new HashMap<String, Object>() {{
                     put("title", "Hi, poi-tl Word模板引擎");
                 }});
         FileOutputStream out = new FileOutputStream("D:\\output.docx");
+        // 输出到流
         template.write(out);
         out.flush();
         out.close();
---
+++ src/main/java/com/learn/chapter2/Test1.java
@@ -0,0 +1,33 @@
+package com.learn.chapter2;
+
+import com.deepoove.poi.XWPFTemplate;
+import com.deepoove.poi.data.HyperLinkTextRenderData;
+import com.deepoove.poi.data.TextRenderData;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+
+/**
+ * @author ：Kristen
+ * @date ：2022/6/7
+ * @description :
+ */
+public class Test1 {
+    public static void main(String[] args) throws IOException {
+        // 编译模板、渲染数据
+        XWPFTemplate template = XWPFTemplate.compile("D:\\test-poitl\\template.docx").render(
+                new HashMap<String, Object>() {{
+                    put("name", "Sayi");
+                    put("author", new TextRenderData("000000", "Sayi"));
+                    put("link", new HyperLinkTextRenderData("website", "http://deepoove.com"));
+                    put("anchor", new HyperLinkTextRenderData("anchortxt", "anchor:appendix1"));
+                }});
+        FileOutputStream out = new FileOutputStream("D:\\test-poitl\\output.docx");
+        // 输出到流
+        template.write(out);
+        out.flush();
+        out.close();
+        template.close();
+    }
+}
