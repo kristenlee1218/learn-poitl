c0997ca94e1a8e832194892b6369267e2727dabc
kristen <1065200748@qq.com>
2022-06-07 16:11:21 +0800

    2022.6.7

--- src/main/java/com/learn/chapter2/Test4.java
+++ src/main/java/com/learn/chapter2/Test4.java
@@ -2,11 +2,13 @@ package com.learn.chapter2;
 
 import com.deepoove.poi.XWPFTemplate;
 import com.deepoove.poi.data.MiniTableRenderData;
+import com.deepoove.poi.data.NumbericRenderData;
 import com.deepoove.poi.data.RowRenderData;
 import com.deepoove.poi.data.TextRenderData;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -14,17 +16,18 @@ import java.util.Map;
 /**
  * @author ：Kristen
  * @date ：2022/6/7
- * @description : pot-tl 操作表格
+ * @description :
  */
-public class Test3 {
+public class Test4 {
     public static void main(String[] args) throws IOException {
         Map<String, Object> data = new HashMap<>();
-        RowRenderData header = RowRenderData.build(new TextRenderData("FF0000", "姓名"), new TextRenderData("FF0000", "学历"));
-
-        RowRenderData row0 = RowRenderData.build("张三", "研究生");
-        RowRenderData row1 = RowRenderData.build("李四", "博士");
-
-        data.put("table", new MiniTableRenderData(header, Arrays.asList(row0, row1)));
+        data.put("list", new NumbericRenderData(new ArrayList<TextRenderData>() {
+            {
+                add(new TextRenderData("Plug-in function, define your own function"));
+                add(new TextRenderData("Supports word text, header..."));
+                add(new TextRenderData("Not just templates"));
+            }
+        }));
         XWPFTemplate template = XWPFTemplate.compile("D:\\test-poitl\\template.docx")
                 .render(data);
         FileOutputStream out;
