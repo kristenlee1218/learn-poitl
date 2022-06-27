a30c725612a7dc9c430ba1401dbcca9dbabd82f4
kristen <1065200748@qq.com>
2022-06-27 15:45:05 +0800

    2022.6.27

--- src/main/java/com/learn/wordTemplate/TestPolicy.java
+++ src/main/java/com/learn/wordTemplate/TestPolicy.java
@@ -21,18 +21,20 @@ import org.openxmlformats.schemas.wordprocessingml.x2006.main.STJc;
  */
 public class TestPolicy extends AbstractRenderPolicy<Object> {
 
-    public static String[] group = new String[]{"政治思想建设", "企业发展质量", "党建工作质量", "作风建设成效"};
-    public static String[][] item = new String[][]{{"政治忠诚", "政治担当", "社会责任"}, {"改革创新", "经营效益", "管理效能", "风险管控"}, {"选人用人", "基层党建", "党风廉政"}, {"团结协作", "联系群众"}};
-    public static String[] value1 = new String[]{"101.0", "102.0", "103.0", "104.0", "105.0", "106.0", "107.0", "108.0", "109.0", "110.0", "111.0", "112.0"};
-    public static String[] value2 = new String[]{"111.0", "112.0", "113.0", "114.0", "115.0", "116.0"};
-    public static String[] totalValue = new String[]{"1110.0", "1120.0", "1130.0", "1140.0", "1150.0", "1160.0"};
-    public static String[] voteType = new String[]{"领导班子成员A1、A2、A3票", "中层测评B票", "职工代表C票", "外部董事A4票"};
-    public static String[] voteTypeGroup = new String[]{"内部测评"};
-    public static String[][] innerEvaluate = new String[][]{{"领导班子成员A1、A2、A3票", "中层测评B票", "职工代表C票"}};
+    //    第一种测试情况（信息中心_2020年第1批综合测评统计报表(1655863588387)）
+//    public static String[] group = new String[]{"政治思想建设", "企业发展质量", "党建工作质量", "作风建设成效"};
+//    public static String[][] item = new String[][]{{"政治忠诚", "政治担当", "社会责任"}, {"改革创新", "经营效益", "管理效能", "风险管控"}, {"选人用人", "基层党建", "党风廉政"}, {"团结协作", "联系群众"}};
+//    public static String[] value1 = new String[]{"101.0", "102.0", "103.0", "104.0", "105.0", "106.0", "107.0", "108.0", "109.0", "110.0", "111.0", "112.0"};
+//    public static String[] value2 = new String[]{"111.0", "112.0", "113.0", "114.0", "115.0", "116.0"};
+//    public static String[] totalValue = new String[]{"1110.0", "1120.0", "1130.0", "1140.0", "1150.0", "1160.0"};
+//    public static String[] voteType = new String[]{"领导班子成员A1、A2、A3票", "中层测评B票", "职工代表C票", "外部董事A4票"};
+//    public static String[] voteTypeGroup = new String[]{"内部测评"};
+//    public static String[][] innerEvaluate = new String[][]{{"领导班子成员A1、A2、A3票", "中层测评B票", "职工代表C票"}};
+//
+//    public static int year = 2022;
+//    public static String depart = "信息中心中心";
 
-    public static int year = 2022;
-    public static String depart = "信息中心中心";
-
+//    第二种测试情况（上海大区公司_2021年第1批综合考核评价统计表）
 //    public static String[] group = new String[]{"政治思想建设", "企业发展质量", "党建工作质量", "作风建设成效"};
 //    public static String[][] item = new String[][]{{"政治忠诚", "政治担当", "社会责任"}, {"改革创新", "经营效益"}, {"选人用人", "基层党建", "党风廉政"}, {"团结协作", "联系群众"}};
 //    public static String[] voteType = new String[]{"董事长(A1)", "总经理(A2)", "其他领导(A3)", "本单位领导班子成员(B1、B2)", "中层经理人(C)", "员工(D)"};
@@ -41,6 +43,16 @@ public class TestPolicy extends AbstractRenderPolicy<Object> {
 //
 //    public static int year = 2022;
 //    public static String depart = "信息中心中心";
+
+    // 第三种测试情况（上海_2021年第1批综合考核评价统计报表(1646623484403)）
+    public static String[] group = new String[]{"政治思想建设", "企业发展质量", "党建工作质量", "作风建设成效"};
+    public static String[][] item = new String[][]{{"政治忠诚", "政治担当", "社会责任"}, {"改革创新", "经营效益"}, {"选人用人", "基层党建", "党风廉政"}, {"团结协作", "联系群众"}};
+    public static String[] voteType = new String[]{"A票", "B票", "C票"};
+    public static String[] voteTypeGroup = new String[]{};
+    public static String[][] innerEvaluate = new String[][]{{}};
+
+    public static int year = 2022;
+    public static String depart = "信息中心中心";
 
     // 计算行
     int row = count(item) + 4;
@@ -126,77 +138,98 @@ public class TestPolicy extends AbstractRenderPolicy<Object> {
 
     // 设置第二、三行标题的样式
     public void setTableHeader(XWPFTable table) {
-        // 处理第二行，先判断第二行的表头内容
-        int length = voteTypeGroup.length + 2;
-        // 判断是否包含外部董事
-        if (isHaveWBDS) {
-            length++;
-        }
-        String[] strHeader1 = new String[length];
-        strHeader1[0] = "评价内容";
-        strHeader1[length - 1] = "全体";
-        if (isHaveWBDS) {
-            strHeader1[length - 2] = "外部董事";
-        }
-        System.arraycopy(voteTypeGroup, 0, strHeader1, 1, voteTypeGroup.length);
-        // 构建第二行
-        RowRenderData header1 = RowRenderData.build(strHeader1);
-        // 垂直合并"全体"
-        TableTools.mergeCellsVertically(table, col - 1, 1, 2);
-        TableTools.mergeCellsVertically(table, col - 2, 1, 2);
-
-        // 是否含有外部董事
-        if (isHaveWBDS) {
-            // 垂直合并“外部董事”
-            TableTools.mergeCellsVertically(table, col - 2 - 2 - 1, 1, 2);
-        }
-        // 垂直合并“评价内容”
-        TableTools.mergeCellsVertically(table, 0, 1, 2);
-
-        // 水平合并“评价内容”
-        TableTools.mergeCellsHorizonal(table, 1, 0, 1);
-        TableTools.mergeCellsHorizonal(table, 2, 0, 1);
-
-        // 水平合并“分组”
-        int start = 1;
-        int end;
-        for (String[] value : innerEvaluate) {
-            end = (value.length + 1) * 2;
-            TableTools.mergeCellsHorizonal(table, 1, start, start + end - 1);
-            start++;
-        }
-
-        // 判断是否含有外部董事
-        if (isHaveWBDS) {
-            // 水平合并“外部董事”
-            TableTools.mergeCellsHorizonal(table, 1, voteTypeGroup.length + 1, voteTypeGroup.length + 2);
-            TableTools.mergeCellsHorizonal(table, 2, col - voteTypeGroup.length - 2 - 1 - 1, col - voteTypeGroup.length - 1 - 1 - 1);
-            // 水平合并“全体”
-            TableTools.mergeCellsHorizonal(table, 1, voteTypeGroup.length + 2, voteTypeGroup.length + 3);
-            TableTools.mergeCellsHorizonal(table, 2, col - voteTypeGroup.length - 2 - 1, col - voteTypeGroup.length - 1 - 1);
-        } else {
-            TableTools.mergeCellsHorizonal(table, 1, voteTypeGroup.length + 1, voteTypeGroup.length + 2);
-            TableTools.mergeCellsHorizonal(table, 2, col - voteTypeGroup.length - 1, col - voteTypeGroup.length);
-        }
-        MiniTableRenderPolicy.Helper.renderRow(table, 1, header1);
-
-        // 处理分组内的票种和小计
-        int total = count(innerEvaluate);
-        int lengthItem = (total + voteTypeGroup.length) + 1;
-        String[] strHeader2 = new String[lengthItem];
-        int index = 1;
-        for (String[] strings : innerEvaluate) {
-            for (String str : strings) {
-                strHeader2[index++] = str;
+        // 含有分组
+        if ((voteTypeGroup != null && voteTypeGroup.length > 0) && (innerEvaluate != null && innerEvaluate.length > 0)) {
+            // 处理第二行，先判断第二行的表头内容
+            int length = voteTypeGroup.length + 2;
+            // 判断是否包含外部董事
+            if (isHaveWBDS) {
+                length++;
             }
-            strHeader2[index++] = "小计";
+            String[] strHeader1 = new String[length];
+            strHeader1[0] = "评价内容";
+            strHeader1[length - 1] = "全体";
+            if (isHaveWBDS) {
+                strHeader1[length - 2] = "外部董事";
+            }
+            System.arraycopy(voteTypeGroup, 0, strHeader1, 1, voteTypeGroup.length);
+            // 构建第二行
+            RowRenderData header1 = RowRenderData.build(strHeader1);
+            // 垂直合并"全体"
+            TableTools.mergeCellsVertically(table, col - 1, 1, 2);
+            TableTools.mergeCellsVertically(table, col - 2, 1, 2);
+
+            // 是否含有外部董事
+            if (isHaveWBDS) {
+                // 垂直合并“外部董事”
+                TableTools.mergeCellsVertically(table, col - 2 - 2, 1, 2);
+            }
+            // 垂直合并“评价内容”
+            TableTools.mergeCellsVertically(table, 0, 1, 2);
+
+            // 水平合并“评价内容”
+            TableTools.mergeCellsHorizonal(table, 1, 0, 1);
+            TableTools.mergeCellsHorizonal(table, 2, 0, 1);
+
+            // 水平合并“分组”
+            int start = 1;
+            int end;
+            for (String[] value : innerEvaluate) {
+                end = (value.length + 1) * 2;
+                TableTools.mergeCellsHorizonal(table, 1, start, start + end - 1);
+                start++;
+            }
+
+            // 判断是否含有外部董事
+            if (isHaveWBDS) {
+                // 水平合并“外部董事”
+                TableTools.mergeCellsHorizonal(table, 1, voteTypeGroup.length + 1, voteTypeGroup.length + 2);
+                TableTools.mergeCellsHorizonal(table, 2, col - voteTypeGroup.length - 2 - 1 - 1, col - voteTypeGroup.length - 1 - 1 - 1);
+                // 水平合并“全体”
+                TableTools.mergeCellsHorizonal(table, 1, voteTypeGroup.length + 2, voteTypeGroup.length + 3);
+                TableTools.mergeCellsHorizonal(table, 2, col - voteTypeGroup.length - 2 - 1, col - voteTypeGroup.length - 1 - 1);
+            } else {
+                TableTools.mergeCellsHorizonal(table, 1, voteTypeGroup.length + 1, voteTypeGroup.length + 2);
+                TableTools.mergeCellsHorizonal(table, 2, col - voteTypeGroup.length - 1, col - voteTypeGroup.length);
+            }
+            MiniTableRenderPolicy.Helper.renderRow(table, 1, header1);
+
+            // 处理分组内的票种和小计
+            int total = count(innerEvaluate);
+            int lengthItem = (total + voteTypeGroup.length) + 1;
+            String[] strHeader2 = new String[lengthItem];
+            int index = 1;
+            for (String[] strings : innerEvaluate) {
+                for (String str : strings) {
+                    strHeader2[index++] = str;
+                }
+                strHeader2[index++] = "小计";
+            }
+            RowRenderData header2 = RowRenderData.build(strHeader2);
+            // 分别给以上几个 TextRenderData 设置合并单元格、第一个格子必须写空值否则无法写入到 table
+            for (int i = 0; i < strHeader2.length - 1; i++) {
+                TableTools.mergeCellsHorizonal(table, 2, i + 1, i + 2);
+            }
+            MiniTableRenderPolicy.Helper.renderRow(table, 2, header2);
+        } else {
+            int length = voteType.length + 2;
+            String[] strHeader1 = new String[length];
+            strHeader1[0] = "评价内容";
+            strHeader1[length - 1] = "全体";
+            System.arraycopy(voteType, 0, strHeader1, 1, voteType.length);
+            // 构建第二行
+            RowRenderData header1 = RowRenderData.build(strHeader1);
+            for (int i = 0; i < voteType.length + 2; i++) {
+                int j = i + 1;
+                TableTools.mergeCellsHorizonal(table, 1, i, j);
+                TableTools.mergeCellsHorizonal(table, 2, i, j);
+            }
+
+            for (int i = voteType.length + 1; i >= 0; i--) {
+                TableTools.mergeCellsVertically(table, i, 1, 2);
+            }
+            MiniTableRenderPolicy.Helper.renderRow(table, 1, header1);
         }
-        RowRenderData header2 = RowRenderData.build(strHeader2);
-        // 分别给以上几个 TextRenderData 设置合并单元格、第一个格子必须写空值否则无法写入到 table
-        for (int i = 0; i < strHeader2.length - 1; i++) {
-            TableTools.mergeCellsHorizonal(table, 2, i + 1, i + 2);
-        }
-        MiniTableRenderPolicy.Helper.renderRow(table, 2, header2);
     }
 
     // 计算所有分组的项的个数
