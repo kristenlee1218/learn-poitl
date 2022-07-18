0efcb52edd4a92d53a8545f251a756bf2408da12
kristen <1065200748@qq.com>
2022-07-18 15:19:20 +0800

    2022.7.18

--- src/main/java/com/learn/renyuanTemplate/TablePolicy.java
+++ src/main/java/com/learn/renyuanTemplate/TablePolicy.java
@@ -14,6 +14,9 @@ import com.deepoove.poi.xwpf.BodyContainerFactory;
 import org.apache.poi.xwpf.usermodel.*;
 import org.openxmlformats.schemas.wordprocessingml.x2006.main.STJc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author ：Kristen
  * @date ：2022/7/12
@@ -24,17 +27,16 @@ public class TablePolicy extends AbstractRenderPolicy<Object> {
     //第一种测试情况（信息中心_2020年第1批综合测评统计报表(1655863588387)）(14)
     public static String[] group = new String[]{"对党忠诚", "勇于创新", "治企有方", "兴企有为", "清正廉洁"};
     public static String[][] item = new String[][]{{"政治品质", "政治本领"}, {"创新精神", "创新成果"}, {"经营管理能力", "抓党建强党建能力"}, {"担当作为", "履职绩效"}, {"一岗双责", "廉洁从业"}};
-    public static String[] people = new String[]{"刘备", "诸葛亮", "关羽", "张飞", "赵云", "黄忠", "马超"};
-    public static String[] career = new String[]{"董事长", "总经理", "副总经理", "副总经理", "副总经理", "总会计师", "党委副书记"};
-    public static int year = 2022;
-    public static String depart = "信息中心";
+//    public static String[] people = new String[]{"刘备", "诸葛亮", "关羽", "张飞", "赵云", "黄忠", "马超"};
+//    public static String[] career = new String[]{"董事长", "总经理", "副总经理", "副总经理", "副总经理", "总会计师", "党委副书记"};
 
     // 计算行和列
-    int col = countCol(item) + 5;
-    int row = people.length + 3;
+    int col = this.countCol(item) + 5;
+    //int row = people.length + 3;
+    int row = 4;
 
     @Override
-    protected void afterRender(RenderContext<Object> renderContext) {
+    public void afterRender(RenderContext<Object> renderContext) {
         // 清空标签
         clearPlaceholder(renderContext, true);
     }
@@ -47,9 +49,10 @@ public class TablePolicy extends AbstractRenderPolicy<Object> {
 
         // 当前位置插入表格
         XWPFTable table = bodyContainer.insertNewTable(run, row, col);
-        setTableStyle(table);
-        setTableTitle(table);
-        setTableHeader(table);
+        this.setTableStyle(table);
+        this.setTableTitle(table);
+        this.setTableHeader(table);
+        this.setTableCellTag(table);
     }
 
     // 整个 table 的样式在此设置
@@ -57,11 +60,11 @@ public class TablePolicy extends AbstractRenderPolicy<Object> {
         // 设置A4幅面的平铺类型和列数
         TableTools.widthTable(table, MiniTableRenderData.WIDTH_A4_NARROW_FULL, col);
         // 设置 border
-        TableTools.borderTable(table, 9);
+        TableTools.borderTable(table, 10);
         for (XWPFTableRow tableRow : table.getRows()) {
             for (int i = 0; i < tableRow.getTableCells().size(); i++) {
                 tableRow.getCell(i).setVerticalAlignment(XWPFTableCell.XWPFVertAlign.CENTER);
-                tableRow.getCell(i).setWidth("1000");
+                tableRow.getCell(i).setWidth("500");
             }
         }
         table.setCellMargins(2, 2, 2, 2);
@@ -77,7 +80,7 @@ public class TablePolicy extends AbstractRenderPolicy<Object> {
         TableStyle style = new TableStyle();
         style.setAlign(STJc.CENTER);
         style.setBackgroundColor("DCDCDC");
-        String title = depart + "领导人员" + year + "年度综合测评汇总表";
+        String title = "{{title}}";
         RowRenderData header0 = RowRenderData.build(new TextRenderData(title, cellStyle));
         header0.setRowStyle(style);
         TableTools.mergeCellsHorizonal(table, 0, 0, col - 1);
@@ -103,7 +106,6 @@ public class TablePolicy extends AbstractRenderPolicy<Object> {
             start++;
         }
 
-
         for (int i = 0; i < group.length; i++) {
             strHeader1[i + 5] = group[i];
             TableTools.mergeCellsVertically(table, i, 1, 2);
@@ -121,14 +123,39 @@ public class TablePolicy extends AbstractRenderPolicy<Object> {
         }
 
         // 构建第二、三行
-        RowRenderData header1 = RowRenderData.build(strHeader1);
-        RowRenderData header2 = RowRenderData.build(strHeader2);
-        TableStyle style = new TableStyle();
-        style.setAlign(STJc.CENTER);
-        header1.setRowStyle(style);
-        header2.setRowStyle(style);
+        Style style = this.getCellStyle();
+        RowRenderData header1 = this.build(style, strHeader1);
+        RowRenderData header2 = this.build(style, strHeader2);
+        TableStyle tableStyle = this.getTableStyle();
+        header1.setRowStyle(tableStyle);
+        header2.setRowStyle(tableStyle);
         MiniTableRenderPolicy.Helper.renderRow(table, 1, header1);
         MiniTableRenderPolicy.Helper.renderRow(table, 2, header2);
+    }
+
+    // 设置数据行的标签
+    public void setTableCellTag(XWPFTable table) {
+        String[] str = new String[col];
+        str[0] = "{{sequence}}";
+        str[1] = "{{leadername}}";
+        str[2] = "{{post}}";
+        str[3] = "{{avg}}";
+        str[4] = "{{sort@avg}}";
+
+        int index = 5;
+        for (int i = 0; i < item.length; i++) {
+            str[index] = "avg#group0" + (i + 1);
+            for (int j = 0; j < item[i].length; j++) {
+                str[++index] = "avg#leader0" + (index - 5 - i);
+            }
+            index++;
+        }
+
+        Style style = this.getDataCellStyle();
+        RowRenderData row = this.build(style, str);
+        TableStyle tableStyle = this.getTableStyle();
+        row.setRowStyle(tableStyle);
+        MiniTableRenderPolicy.Helper.renderRow(table, 3, row);
     }
 
     // 计算所有分组的项的个数
@@ -142,5 +169,41 @@ public class TablePolicy extends AbstractRenderPolicy<Object> {
             }
         }
         return count + index;
+    }
+
+    // 根据 String[] 构建一行的数据，同一行使用一个 Style
+    public RowRenderData build(Style style, String... cellStr) {
+        List<TextRenderData> data = new ArrayList<>();
+        if (null != cellStr) {
+            for (String col : cellStr) {
+                data.add(new TextRenderData(col, style));
+            }
+        }
+        return new RowRenderData(data, null);
+    }
+
+    // 设置 cell 格样式
+    public Style getCellStyle() {
+        Style cellStyle = new Style();
+        cellStyle.setFontFamily("宋体");
+        cellStyle.setFontSize(10);
+        cellStyle.setColor("000000");
+        return cellStyle;
+    }
+
+    // 设置 table 格样式
+    public TableStyle getTableStyle() {
+        TableStyle tableStyle = new TableStyle();
+        tableStyle.setAlign(STJc.CENTER);
+        return tableStyle;
+    }
+
+    // 设置 cell 格样式
+    public Style getDataCellStyle() {
+        Style cellStyle = new Style();
+        cellStyle.setFontFamily("宋体");
+        cellStyle.setFontSize(8);
+        cellStyle.setColor("000000");
+        return cellStyle;
     }
 }
