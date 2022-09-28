0d354df1b199a4bb45028e3bc08459e6f8693bb3
kristen <1065200748@qq.com>
2022-09-28 16:33:31 +0800

    2022.9.28

--- src/main/java/com/learn/countTemplate/NewLeaderPolicy.java
+++ src/main/java/com/learn/countTemplate/NewLeaderPolicy.java
@@ -25,9 +25,11 @@ import java.util.List;
  */
 public class NewLeaderPolicy extends AbstractRenderPolicy<Object> {
 
-    public static String[] voteType = new String[]{"A1", "A2", "A3", "B", "C"};
-    public static String option = "2:极差:0;4:不了解:0;6:不好:0;8:一般:0;10:好:0";
+    public static String[] voteType = new String[]{"A1/A2", "A3", "B", "C"};
+    public static String option = "4:不了解:0;6:不认同:0;8:基本认同:0;10:认同:0";
+    public static String[] question = new String[]{"对提拔任用该干部的看法"};
     public static String[] properties = new String[]{"序号", "姓名", "出生年月", "原任职务", "现任职务", "任职时间"};
+    public static String[] data = new String[]{};
 
     // 计算行和列
     int col;
@@ -45,12 +47,16 @@ public class NewLeaderPolicy extends AbstractRenderPolicy<Object> {
         XWPFRun run = renderContext.getRun();
         // 当前位置的容器
         BodyContainer bodyContainer = BodyContainerFactory.getBodyContainer(run);
-
+        // 计算行列
+        optionMap = this.splitOption(option);
+        row = 5 + data.length;
+        col = properties.length + voteType.length * optionMap.size() + (optionMap.size() + 1) * 2;
         // 当前位置插入表格
         XWPFTable table = bodyContainer.insertNewTable(run, row, col);
         this.setTableStyle(table);
         this.setTableTitle(table);
         this.setTableHeader(table);
+        //this.setTableTag(table);
     }
 
     // 整个 table 的样式在此设置
@@ -87,7 +93,113 @@ public class NewLeaderPolicy extends AbstractRenderPolicy<Object> {
     }
 
     public void setTableHeader(XWPFTable table) {
+        // 第1行值的数组
+        String[] strHeader1 = new String[2];
+        strHeader1[0] = "被评议对象的基本情况";
+        strHeader1[1] = question[0];
 
+        // 第1行水平合并单元格 对提拔任用该干部的看法部分
+        TableTools.mergeCellsHorizonal(table, 1, properties.length, col - 1);
+        // 第1行水平合并单元格 被评议对象的基本情况部分
+        TableTools.mergeCellsHorizonal(table, 1, 0, properties.length - 1);
+        TableTools.mergeCellsHorizonal(table, 2, 0, properties.length - 1);
+
+        // 第1行垂直合并单元格
+        TableTools.mergeCellsVertically(table, 0, 1, 2);
+
+        // 第2行值的数组
+        String[] strHeader2 = new String[voteType.length + 2];
+        System.arraycopy(voteType, 0, strHeader2, 1, voteType.length);
+        strHeader2[strHeader2.length - 1] = "合计";
+
+        // 第2行水平合并单元格 票种部分
+        int startHeader2 = 1;
+        int endHeader2;
+        for (int i = 0; i < strHeader2.length - 2; i++) {
+            endHeader2 = startHeader2 + optionMap.size() - 1;
+            TableTools.mergeCellsHorizonal(table, 2, startHeader2, endHeader2);
+            startHeader2++;
+        }
+
+        // 第2行水平合并单元格 合计部分
+        TableTools.mergeCellsHorizonal(table, 2, startHeader2, startHeader2 + 2 * (optionMap.size() + 1) - 1);
+
+        // 第3行值的数组
+        String[] strHeader3 = new String[properties.length + (optionMap.size()) * (voteType.length + 1) + 1];
+        System.arraycopy(properties, 0, strHeader3, 0, properties.length);
+        int header3Index = properties.length;
+        for (int i = 0; i < voteType.length + 1; i++) {
+            for (int j = 0; j < optionMap.keySet().toArray().length; j++) {
+                strHeader3[header3Index++] = optionMap.keySet().toArray()[j].toString();
+            }
+        }
+        strHeader3[strHeader3.length - 1] = "认同度";
+
+        // 第3行水平合并单元格 合计部分
+        int startHeader3 = voteType.length * optionMap.size() + properties.length;
+        for (int i = 0; i < optionMap.size(); i++) {
+            TableTools.mergeCellsHorizonal(table, 3, startHeader3, startHeader3 + 1);
+            startHeader3++;
+        }
+
+        // 第3行水平合并单元格 认同度部分
+        TableTools.mergeCellsHorizonal(table, 3, startHeader3, startHeader3 + 1);
+
+        // 第3行垂直合并单元格 属性部分
+        for (int i = 0; i < properties.length; i++) {
+            TableTools.mergeCellsVertically(table, i, 3, 4);
+        }
+
+        // 第4行数组的值
+        String[] strHeader4 = new String[col];
+        for (int i = 0; i < voteType.length * optionMap.size(); i++) {
+            strHeader4[i + properties.length] = "得票";
+        }
+
+        // 第4行值的数组 合计部分
+        for (int i = voteType.length * optionMap.size() + properties.length; i < strHeader4.length; i++) {
+            if ((voteType.length * optionMap.size()) % 2 == 0) {
+                if (i % 2 == 0) {
+                    strHeader4[i] = "得票";
+                } else {
+                    strHeader4[i] = "比例";
+                }
+            } else {
+                if (i % 2 == 0) {
+                    strHeader4[i] = "比例";
+                } else {
+                    strHeader4[i] = "得票";
+                }
+            }
+        }
+        strHeader4[strHeader4.length - 1] = "排名";
+
+        // 构建第1-4行
+        Style style = this.getCellStyle();
+        RowRenderData header1 = this.build(strHeader1, style);
+        RowRenderData header2 = this.build(strHeader2, style);
+        RowRenderData header3 = this.build(strHeader3, style);
+        RowRenderData header4 = this.build(strHeader4, style);
+        TableStyle tableStyle = this.getTableStyle();
+        header1.setRowStyle(tableStyle);
+        header2.setRowStyle(tableStyle);
+        header3.setRowStyle(tableStyle);
+        header4.setRowStyle(tableStyle);
+        MiniTableRenderPolicy.Helper.renderRow(table, 1, header1);
+        MiniTableRenderPolicy.Helper.renderRow(table, 2, header2);
+        MiniTableRenderPolicy.Helper.renderRow(table, 3, header3);
+        MiniTableRenderPolicy.Helper.renderRow(table, 4, header4);
+    }
+
+    // 将题目的选项 如：“4:不了解:0;6:不认同:0;8:基本认同:0;10:认同:0” 存入 map，key 为显示的值，value 为分值
+    public LinkedHashMap<String, Integer> splitOption(String option) {
+        LinkedHashMap<String, Integer> optionMap = new LinkedHashMap<>();
+        String[] strArray = option.replaceAll(":0", "").split(";");
+        for (int i = strArray.length - 1; i >= 0; i--) {
+            String[] str = strArray[i].split(":");
+            optionMap.put(str[1], Integer.valueOf(str[0]));
+        }
+        return optionMap;
     }
 
     // 根据 String[] 构建一行的数据，同一行使用一个 Style
--- src/main/java/com/learn/countTemplate/SelectPeoplePolicy1.java
+++ src/main/java/com/learn/countTemplate/SelectPeoplePolicy1.java
@@ -26,10 +26,10 @@ import java.util.*;
  */
 public class SelectPeoplePolicy1 extends AbstractRenderPolicy<Object> {
 
-    //    public static String[] voteType = new String[]{"A1/A2", "A3", "B", "C"};
-//    public static String option = "4:不了解:0;6:不好:0;8:一般:0;10:好:0";
+    public static String[] voteType = new String[]{"A1/A2", "A3", "B", "C"};
+    //    public static String option = "4:不了解:0;6:不好:0;8:一般:0;10:好:0";
 //    public static String[] question = new String[]{"1、对本单位选人用人工作的总体评价", "2、对本单位从严管理监督干部情况的评价"};
-    public static String[] voteType = new String[]{"A1", "A2", "A3", "B", "C"};
+    //public static String[] voteType = new String[]{"A1", "A2", "A3", "B", "C"};
     public static String option = "2:极差:0;4:不了解:0;6:不好:0;8:一般:0;10:好:0";
     public static String[] question = new String[]{"1、对本单位选人用人工作的总体评价"};
 
